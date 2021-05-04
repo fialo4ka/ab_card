0b5407718340a087771da70fd8bfc20070bd3284
Antonina Vracinska <VracinskaA@b2b.regn.net>
2021-05-04 13:56:07 +0200

    init

---
+++ src/Theme/CommonColor.js
@@ -0,0 +1,13 @@
+import { Platform, Dimensions, PixelRatio } from 'react-native';
+
+let white = '#fff';
+let mainDark = '#f00';
+
+
+function mainDarkColor() {
+      return mainDark;
+}
+function whitekColor() {
+  return white;
+}
+export { mainDarkColor, whitekColor }
---
+++ src/Theme/Style.js
@@ -0,0 +1,18 @@
+import { StyleSheet} from 'react-native';
+import * as variables from './CommonColor';
+
+
+const footerTextConstatn = {
+
+};
+const footerButtonConstatn = {
+
+};
+
+export const styles = StyleSheet.create({
+  h1: {
+    ...footerButtonConstatn,
+    color: variables.mainDarkColor(),
+    backgroundColor: "#ff0",
+  },
+});
